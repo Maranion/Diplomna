238d357aaed48c15e2fcf4452b078147a4233a66
ivan likov <TheLikov2003@gmail.com>
2021-12-22 12:14:58 +0200

    added admin side superuser. added one_day_value model to admin

--- mysite/crypto_investment_simulator/admin.py
+++ mysite/crypto_investment_simulator/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import one_day_value
 
-# Register your models here.
+
+admin.site.register(one_day_value)
--- mysite/crypto_investment_simulator/models.py
+++ mysite/crypto_investment_simulator/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class one_day_value(models.Model):
-    date = models.DateTimeField('date of change')
+    date = models.DateField('date of change')
     close_value = models.FloatField()
     symbol = models.CharField(max_length=12)
