07e1460f47bce5ee259e449e1a6675926e074b50
ivan likov <TheLikov2003@gmail.com>
2021-12-22 12:26:19 +0200

    added a basic html page to index view

--- mysite/crypto_investment_simulator/views.py
+++ mysite/crypto_investment_simulator/views.py
@@ -1,6 +1,10 @@
+from django import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import one_day_value
 
 
 def index(request):
-    return HttpResponse('malincho')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
