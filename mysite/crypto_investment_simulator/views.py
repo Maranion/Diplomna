39de5cc92aba4046be430b706861457258f569bc
ivan likov <TheLikov2003@gmail.com>
2022-01-04 11:46:46 +0200

    Added a function that can read csv files and can convert unix time to human readable format

--- mysite/crypto_investment_simulator/views.py
+++ mysite/crypto_investment_simulator/views.py
@@ -3,8 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import one_day_value
+import csv, pandas as pd, os
 
+
+coins_list = ['gemini_BTCUSD_day.csv', 'gemini_LTCUSD_day.csv']
+
+def open_csv(path):
+    csv_file = pd.read_csv(path)
+    csv_file['Unix Timestamp'] = pd.to_datetime(csv_file['Unix Timestamp'], unit = 'ms')
+    print(csv_file)
 
 def index(request):
     template = loader.get_template('index.html')
+    for coin in coins_list:
+        open_csv(f'{os.getcwd()}\\{coin}')
+
     return HttpResponse(template.render())
